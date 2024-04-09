f63737a350f3f3eced3b5dec02e7f6be8b13ab3f
andreyR05 <andrey.rodrigues@sptech.shool>
2024-04-09 20:25:50 -0300

    feat: add queries to use in controller

--- src/main/java/school/sptech/crudrisecanvas/repositories/UserRepositary.java
+++ src/main/java/school/sptech/crudrisecanvas/repositories/UserRepositary.java
@@ -1,8 +1,17 @@
 package school.sptech.crudrisecanvas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import school.sptech.crudrisecanvas.entities.User;
 
 public interface UserRepositary extends JpaRepository<User, Integer>{
+    @Query("SELECT count(*) FROM User u WHERE u.email = ?1 OR u.cpf = ?2")
+    Integer countWithEmailOrCpf(String email, String cpf);
+
+    @Query("SELECT count(*) FROM User u WHERE u.id <> ?3 and (u.email = ?1 OR u.cpf = ?2)")
+    Integer countWithEmailOrCpfAndDiferentId(String email, String cpf, Integer id);
+     
+    @Query("SELECT count(*) FROM User u WHERE u.id = ?1")
+    Integer countWithId(Integer id);
 }

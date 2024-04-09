2ebbabb6abf47554e91b34b81b7ae3aac6e87392
andreyR05 <andrey.rodrigues@sptech.shool>
2024-04-09 16:50:12 -0300

    refactor: remove infos in ongStatus

--- src/main/java/school/sptech/crudrisecanvas/Utils/Enums/OngStatus.java
+++ src/main/java/school/sptech/crudrisecanvas/Utils/Enums/OngStatus.java
@@ -1,13 +1,7 @@
 package school.sptech.crudrisecanvas.Utils.Enums;
 
 public enum OngStatus {
-    ACPECTED("aceito"),
-    PENDING("pendente"),
-    REJECTED("rejeitado");
-
-    private String status;
-
-    OngStatus(String status) {
-        this.status = status;
-    }
+    ACPECTED,
+    PENDING,
+    REJECTED;
 }

56f5467d3cb7923c76636b3bbfc622e6870ae763
AndreyR05 <andrey.rodrigues@sptech.school>
2024-05-12 17:20:34 -0300

    refactor: remove status from userRequestDto

--- src/main/java/school/sptech/crudrisecanvas/dtos/mapping/MappingRequestDto.java
+++ src/main/java/school/sptech/crudrisecanvas/dtos/mapping/MappingRequestDto.java
@@ -1,15 +1,21 @@
 package school.sptech.crudrisecanvas.dtos.mapping;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
-import school.sptech.crudrisecanvas.utils.Enums.MappingStatus;
 
-// TODO: need validation
 @Data
 public class MappingRequestDto {
+    @Positive
     private Integer qtyPeople;
     private String description;
+
+    @DecimalMax("90")
+    @DecimalMin("-90")
     private Double latitude;
+
+    @DecimalMax("180")
+    @DecimalMin("-180")
     private Double longitude;
-    //TODO: need remove status here, only will setted in service
-    private MappingStatus status;
 }

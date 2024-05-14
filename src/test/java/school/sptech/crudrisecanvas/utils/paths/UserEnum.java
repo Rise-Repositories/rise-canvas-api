44c5a289186f6ec51f91e7a831ce7498f4e2417b
danielyuzo <daniel.palma@sptech.school>
2024-05-14 00:10:31 -0300

    feat: Unit Tests for User Update and Delete

--- src/test/java/school/sptech/crudrisecanvas/utils/paths/UserEnum.java
+++ src/test/java/school/sptech/crudrisecanvas/utils/paths/UserEnum.java
@@ -5,7 +5,7 @@ public enum UserEnum {
     BASE_URI("%s/user".formatted(BaseEnum.BASE_PATH.path)),
     LOGIN("%s/auth/login".formatted(BASE_URI.path)),
     CREATE("%s/auth/register".formatted(BASE_URI.path)),
-    DELETE("%s/".formatted(BASE_URI.path));
+    BY_ID("%s/".formatted(BASE_URI.path));
 
     UserEnum(String path) { this.path = path; }
 

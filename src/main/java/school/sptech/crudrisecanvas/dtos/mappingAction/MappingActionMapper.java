f47da81d190d18d21fca70d8f5db26316677f61a
AndreyR05 <andrey.rodrigues@sptech.school>
2024-05-13 21:23:46 -0300

    feat: add mapping and action for action response

--- src/main/java/school/sptech/crudrisecanvas/dtos/mappingAction/MappingActionMapper.java
+++ src/main/java/school/sptech/crudrisecanvas/dtos/mappingAction/MappingActionMapper.java
@@ -1,5 +1,7 @@
 package school.sptech.crudrisecanvas.dtos.mappingAction;
 
+import java.util.List;
+
 import school.sptech.crudrisecanvas.dtos.action.ActionMapper;
 import school.sptech.crudrisecanvas.dtos.mapping.MappingMapper;
 import school.sptech.crudrisecanvas.entities.MappingAction;
@@ -33,12 +35,20 @@ public class MappingActionMapper {
     public static MappingActionResponseDto toNoRelationDto(MappingAction mappingAction) {
         MappingActionResponseDto dto = new MappingActionResponseDto();
         dto.setId(mappingAction.getId());
+        dto.setAction(ActionMapper.toNoRelation(mappingAction.getAction()));
+        dto.setMapping(MappingMapper.toNoRelationDto(mappingAction.getMapping()));
         dto.setQtyServedAdults(mappingAction.getQtyServedAdults());
         dto.setQtyServedChildren(mappingAction.getQtyServedChildren());
         dto.setNoDonation(mappingAction.getNoDonation());
         dto.setNoPeople(mappingAction.getNoPeople());
         dto.setDescription(mappingAction.getDescription());
         return dto;
+    }
+
+    public static List<MappingActionResponseDto> toNoRelationDto(List<MappingAction> mappingAction) {
+        return mappingAction == null
+                ? null
+                : mappingAction.stream().map(MappingActionMapper::toNoRelationDto).toList();
     }
     
 }

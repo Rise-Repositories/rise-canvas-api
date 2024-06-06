a49974ee3aeef215b9899626a6a0bca22174a853
danielyuzo <daniel.palma@sptech.school>
2024-06-05 22:58:49 -0300

    fix: normalizing heatmap data return

--- src/main/java/school/sptech/crudrisecanvas/Utils/HeatmapGenerator.java
+++ src/main/java/school/sptech/crudrisecanvas/Utils/HeatmapGenerator.java
@@ -22,6 +22,7 @@ public class HeatmapGenerator {
         } else {
             Double[][] points = new Double[mappings.size()][3];
             int qtyPoints = 0;
+            Double maxIntensity = 0.;
 
 //            List<Mapping> validMappings = mappings.stream()
 //                .filter((map) -> map.getMappingActions().isEmpty() || !map.getMappingActions().stream()
@@ -44,6 +45,7 @@ public class HeatmapGenerator {
                             points[j][0], points[j][1]
                     ) <= radiusToGroup) {
                         points[j][2] += curMap.getQtyAdults() + curMap.getQtyChildren();
+                        maxIntensity = Math.max(maxIntensity, points[j][2]);
                         newPoint = false;
                         break;
                     }
@@ -53,12 +55,18 @@ public class HeatmapGenerator {
                     points[qtyPoints][0] = curMap.getLatitude();
                     points[qtyPoints][1] = curMap.getLongitude();
                     points[qtyPoints][2] = curMap.getQtyAdults().doubleValue() + curMap.getQtyChildren().doubleValue();
-                     qtyPoints++;
+
+                    maxIntensity = Math.max(maxIntensity, points[qtyPoints][2]);
+                    qtyPoints++;
                 }
             }
 
-            // TODO normalize return
-            return Arrays.copyOfRange(points, 0, qtyPoints);
+            Double finalMaxIntensity = maxIntensity;
+            points = Arrays.stream(Arrays.copyOfRange(points, 0, qtyPoints))
+                    .peek((heatData) -> heatData[2] = (heatData[2] * 0.9 / finalMaxIntensity) + 0.1)
+                    .toArray(size -> new Double[size][3]);
+
+            return points;
         }
     }
 

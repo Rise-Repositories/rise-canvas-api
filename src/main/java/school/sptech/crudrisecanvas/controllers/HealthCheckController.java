aff44aa428ee4cd73b4ccc379b1fcc36fa7cf336
AndreyR05 <andrey.rodrigues@sptech.school>
2024-12-03 18:10:19 -0300

    feat: healtcheck

---
+++ src/main/java/school/sptech/crudrisecanvas/controllers/HealthCheckController.java
@@ -0,0 +1,16 @@
+package school.sptech.crudrisecanvas.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+    
+}
